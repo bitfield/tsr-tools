cd6408f6814c0844b74cc6fae9ca343be0cf987e
Jakub Jarosz <jakub@jarosz.dev>
2024-09-27 14:54:38 +0100

    Append to a file

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -1,11 +1,29 @@
+use anyhow::anyhow;
+use std::env;
 use std::fs::OpenOptions;
 use std::io::Write;
 
+// logbook 1
+// fn main() -> anyhow::Result<()> {
+//     let mut logbook = OpenOptions::new()
+//         .create(true)
+//         .append(true)
+//         .open("logbook.txt")?;
+//     writeln!(logbook, "Hello, logbook!")?;
+//     Ok(())
+// }
+
+// logbook 2
 fn main() -> anyhow::Result<()> {
+    let line = env::args().skip(1).collect::<Vec<_>>().join(" ");
+    if line.is_empty() {
+        return Err(anyhow!("Usage: logbook <LINE>"));
+    }
+
     let mut logbook = OpenOptions::new()
         .create(true)
         .append(true)
         .open("logbook.txt")?;
-    writeln!(logbook, "Hello, logbook!")?;
+    writeln!(logbook, "{line}")?;
     Ok(())
 }
