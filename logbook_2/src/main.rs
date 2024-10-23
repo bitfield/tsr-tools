7a51b9be302115516ac5c824c320538ba8c51416
John Arundel <john@bitfieldconsulting.com>
2024-10-23 11:08:20 +0100

    tweak usage message

--- logbook_2/src/main.rs
+++ logbook_2/src/main.rs
@@ -6,7 +6,7 @@ use std::{env, fs::File};
 fn main() -> anyhow::Result<()> {
     let args: Vec<_> = env::args().skip(1).collect();
     if args.is_empty() {
-        return Err(anyhow!("Usage: logbook <LINE>"));
+        return Err(anyhow!("Usage: logbook <MESSAGE>"));
     }
     let mut logbook = File::options()
         .create(true)
