2987408c923b0043c8b556f6164562745ac2b335
John Arundel <john@bitfieldconsulting.com>
2024-12-27 17:35:29 +0000

    reset scratch

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -1,35 +1,3 @@
-use anyhow::Result;
-use clap::Parser;
-
-use scratch as weather;
-use weather::Weatherstack;
-
-#[derive(Parser)]
-/// Shows the current weather for a given location.
-struct Args {
-    #[arg(short, long, env = "WEATHERSTACK_API_KEY", required = true)]
-    /// Weatherstack API key
-    api_key: String,
-    #[arg(short, long)]
-    /// Report temperatures in Fahrenheit
-    fahrenheit: bool,
-    #[arg(required = true)]
-    /// Example: "London,UK"
-    location: Vec<String>,
-}
-
-fn main() -> Result<()> {
-    let args = Args::parse();
-    let location = args.location.join(" ");
-    let ws = Weatherstack::new(&args.api_key);
-    let weather = ws.get_weather(&location)?;
-    println!(
-        "{} {}", weather.summary,
-        if args.fahrenheit {
-            format!("{:.1}ºF", weather.temperature.as_fahrenheit())
-        } else {
-            format!("{:.1}ºC", weather.temperature.as_celsius())
-        }
-    );
-    Ok(())
+fn main() {
+    println!("Hello, world");
 }
